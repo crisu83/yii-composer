e4b0e52cd26f1b447cc67663b764f9f21033b33f
Christoffer Niska <ChristofferNiska@gmail.com>
2013-07-24 23:35:10 +0300

    Update InstallHander.php

--- src/Crisu83/YiiComposer/InstallHandler.php
+++ src/Crisu83/YiiComposer/InstallHandler.php
@@ -1,12 +1,11 @@
 <?php
-/**
- * InstallHandler class file.
- * @author Christoffer Niska <christoffer.niska@gmail.com>
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @author Tobias Munk <schmunk@usrbin.de>
- * @copyright Copyright &copy; Christoffer Niska 2013-
- * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
- * @package Crisu83.YiiComposer;
+/*
+ * This file is part of YiiComposer.
+ *
+ * (c) 2013 Christoffer Niska
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Crisu83\YiiComposer;
@@ -15,11 +14,15 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 defined('STDIN') or define('STDIN', fopen('php://stdin', 'r'));
 
-use Composer\Script\CommandEvent;
+use Composer\Script\Event;
 
 /**
  * InstallHandler is called by Composer after it installs/updates the current package.
  * Ported from https://github.com/yiisoft/yii2-composer/ and adapted to Yii.
+ *
+ * @author Christoffer Niska <christoffer.niska@gmail.com>
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @author Tobias Munk <schmunk@usrbin.de>
  */
 class InstallHandler
 {
@@ -28,9 +31,9 @@ class InstallHandler
 
     /**
      * Sets the correct permissions of files and directories.
-     * @param CommandEvent $event
+     * @param Event $event
      */
-    public static function setPermissions($event)
+    public static function setPermissions(Event $event)
     {
         $options = array_merge(
             array(
